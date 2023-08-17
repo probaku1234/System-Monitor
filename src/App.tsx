1423b417ababd4e0c818eba651490d78b2f4be7c
Yun Seob Lee <fhzotxldj@gmail.com>
2023-08-17 13:50:53 +0900

    add pie chart and gauge chart

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import { NavSidebar } from "./components/NavSideBar";
 import "./App.css";
+import { PieChart } from "./components/PieChart";
+import Gauge from "react-svg-gauge"
 
 type SystemInfo = {
   cpu_name: string;
@@ -13,10 +15,19 @@ type SystemInfo = {
   gpu_load: number;
 };
 
+function getHexColor(value: number) {
+  let string = value.toString(16);
+  return (string.length === 1) ? '0' + string : string;
+}
+
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
   const [systemInfo, setSystemInfo] = useState({} as SystemInfo);
+  let r = Math.floor(60 * 2.55);
+  let g = Math.floor(255 - (60 * 2.55));
+  let b = 0;
+  let colorHex = '#' + getHexColor(r) + getHexColor(g) + getHexColor(b);
 
   useEffect(() => {
     fetchSystemInfo();
@@ -79,6 +90,14 @@ function App() {
           <p>gpu name: {systemInfo?.gpu_name}</p>
           <p>gpu load: {systemInfo?.gpu_load}</p>
           <p>gpu temp: {systemInfo?.gpu_temp}</p>
+        </div>
+
+        <div>
+          <h1>GPU</h1>
+          <PieChart 
+            data={1}
+          />
+          <Gauge value={60} width={400} height={320} color={colorHex} label="This is a big one" valueFormatter={value => `${value}%`} />
         </div>
       </div>
     </>
---
+++ src/components/PieChart.tsx
@@ -0,0 +1,58 @@
+import { VictoryPie, VictoryAnimation, VictoryLabel } from "victory";
+import React from "react";
+
+type PiChartProps = {
+  data: number;
+};
+
+export const PieChart: React.FC<PiChartProps> = ({ data }) => {
+  return (
+    <div>
+      <svg viewBox="0 0 400 400" width="100%" height="100%">
+        <VictoryPie
+          standalone={false}
+          animate={{ duration: 1000 }}
+          width={400}
+          height={400}
+          data={[
+            { x: 1, y: data },
+            { x: 2, y: 100 - data },
+          ]}
+          innerRadius={120}
+          cornerRadius={25}
+          labels={() => null}
+          style={{
+            data: {
+              fill: ({ datum }) => {
+                const color = datum.y > 30 ? "green" : "red";
+                return datum.x === 1 ? color : "transparent";
+              },
+            },
+          }}
+        />
+        <VictoryAnimation
+          duration={1000}
+          data={[
+            { x: 1, y: data },
+            { x: 2, y: 100 - data },
+          ]}
+        >
+          {(newProps) => {
+            return (
+              <VictoryLabel
+                textAnchor="middle"
+                verticalAnchor="middle"
+                x={200}
+                y={200}
+                text={`${data}%`}
+                style={{ fontSize: 45 }}
+              />
+            );
+          }}
+        </VictoryAnimation>
+      </svg>
+    </div>
+  );
+};
+
+export default PieChart;
