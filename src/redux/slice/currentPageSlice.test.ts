f5aef903db853a7dea133cdb3a0f0b79b921dc58
Yun Seob Lee <fhzotxldj@gmail.com>
2023-08-18 15:25:37 +0900

    add test file for current page slice

--- src/redux/slice/currentPageSlice.test.ts
+++ src/redux/slice/currentPageSlice.test.ts
@@ -1,9 +1,25 @@
-import currentPageReducer from "./currentPageSlice";
+import currentPageReducer, {
+  setCurrentPage,
+  CurrentPageState,
+} from "./currentPageSlice";
+import { it, describe, expect } from "vitest";
 
 describe("current page reducer", () => {
-  it("should work", () => {
-    expect(currentPageReducer(undefined, { type: "unkown" })).toEqual({
-      value: 0,
+  const initialState: CurrentPageState = {
+    value: "home",
+  };
+
+  it("should handle initial state", () => {
+    expect(currentPageReducer(undefined, { type: "unknown" })).toEqual({
+      value: "home",
     });
+  });
+
+  it("should handle update state", () => {
+    const actual = currentPageReducer(
+      initialState,
+      setCurrentPage("monitoring")
+    );
+    expect(actual.value).toEqual("monitoring");
   });
 });
