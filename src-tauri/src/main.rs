2a0d08dab5d7e3c880442133eb7cba636aad24fd
Yun Seob Lee <fhzotxldj@gmail.com>
2023-08-21 18:42:58 +0900

    refactor use new system info fetcher struct

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::process::Command;
-use std::thread;
-use std::time::Duration;
-use systemstat::{saturating_sub_bytes, Platform, System};
+use crate::sys_info::SystemInfoFetcher;
 
 mod sys_info;
 
@@ -16,88 +13,35 @@ fn greet(name: &str) -> String {
 
 #[tauri::command]
 fn get_sys_info() -> sys_info::SystemInfo {
-    let sys = System::new();
+    let system_info_fetcher = SystemInfoFetcher::new();
     let mut used_memory: u64 = 0;
     let mut total_memory: u64 = 0;
 
-    match sys.memory() {
-        Ok(mem) => {
-            used_memory = saturating_sub_bytes(mem.total, mem.free).as_u64();
-            total_memory = mem.total.as_u64();
-            println!(
-                "\nMemory: {} used / {} ({} bytes) total ({:?})",
-                saturating_sub_bytes(mem.total, mem.free),
-                mem.total,
-                mem.total.as_u64(),
-                mem.platform_memory
-            )
-        }
-        Err(x) => println!("\nMemory: error: {}", x),
-    }
+    let memory_info = system_info_fetcher.memory_info();
+    used_memory = memory_info.0;
+    total_memory = memory_info.1;
 
-    match sys.cpu_load_aggregate() {
-        Ok(cpu) => {
-            println!("\nMeasuring CPU load...");
-            thread::sleep(Duration::from_secs(1));
-            let cpu = cpu.done().unwrap();
-            println!(
-                "CPU load: {}% user, {}% nice, {}% system, {}% intr, {}% idle ",
-                cpu.user * 100.0,
-                cpu.nice * 100.0,
-                cpu.system * 100.0,
-                cpu.interrupt * 100.0,
-                cpu.idle * 100.0
-            );
-        }
-        Err(x) => println!("\nCPU load: error: {}", x),
-    }
+    let gpu_info = system_info_fetcher.gpu_info();
 
-    match sys.cpu_temp() {
-        Ok(cpu_temp) => println!("\nCPU temp: {}", cpu_temp),
-        Err(x) => println!("\nCPU temp: {}", x),
-    }
+    let gpu_temp: i8 = gpu_info.0;
+    let gpu_name: String = gpu_info.1;
+    let gpu_load: i8 = gpu_info.2;
 
-    let output = Command::new("cmd")
-                            .args(["/c", "nvidia-smi --query-gpu=temperature.gpu,gpu_name,utilization.gpu --format=csv,noheader,nounits"])
-                            .output()
-                            .expect("msg");
-    println!("{}", String::from_utf8_lossy(&output.stdout));
+    let cpu_name = system_info_fetcher.cpu_name();
 
-    let gpu_result_text = String::from_utf8_lossy(&output.stdout);
-    let gpu_result_string = gpu_result_text.to_string();
-    let gpu_info_vec: Vec<&str> = gpu_result_string.split(",").collect();
-    let gpu_temp: i8 = gpu_info_vec[0].trim().parse().unwrap();
-    let gpu_name: String = gpu_info_vec[1].trim().to_string();
-    let gpu_load: i8 = gpu_info_vec[2].trim().parse().unwrap();
-
-    for part in gpu_result_string.split(", ") {
-        println!("{}", part);
-    }
-
-    let pika = Command::new("cmd")
-        .args(["/c", "wmic cpu get name"])
-        .output()
-        .expect("msg");
-    println!("{}", String::from_utf8_lossy(&pika.stdout));
-
-    let result_text = String::from_utf8_lossy(&pika.stdout);
-    let result_string = result_text.to_string();
-    let mut lines = result_string.lines();
-    lines.next();
-    let cpu_name = lines.next().unwrap();
-
-    sys_info::SystemInfo::new(
+    sys_info::SystemInfo {
         used_memory,
         total_memory,
-        cpu_name.to_string(),
+        cpu_name: cpu_name.to_string(),
         gpu_temp,
         gpu_name,
         gpu_load,
-    )
+    }
 }
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::default().build())
         .invoke_handler(tauri::generate_handler![greet, get_sys_info])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
