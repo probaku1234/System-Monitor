9a78f5111a4be56e8e9f1eb7a4132128cc93b3ca
Yun Seob Lee <fhzotxldj@gmail.com>
2023-08-21 18:19:53 +0900

    update monitoring page to add cpu gpu chart

--- src/pages/Monitoring.tsx
+++ src/pages/Monitoring.tsx
@@ -1,11 +1,25 @@
 import { SystemInfoChart } from "../components/SystemInfoChart";
+import { useAppSelector } from "../redux/hooks";
+import { selectSystemInfo } from "../redux/slice/systemInfoSlice";
 
 function Monitoring() {
+  const { gpuLoad: gpu_load, gpuTemp: gpu_temp } =
+    useAppSelector(selectSystemInfo);
+
   return (
     <>
       <h1>Monitoring</h1>
 
-      <SystemInfoChart />
+      <SystemInfoChart 
+        type="GPU"
+        load={gpu_load}
+        temp={gpu_temp}
+      />
+      <SystemInfoChart 
+        type="CPU"
+        load={gpu_load}
+        temp={gpu_temp}
+      />
     </>
   );
 }
