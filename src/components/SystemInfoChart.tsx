ddeea3a0c9562e822a6d0b76512a22b3dff36052
Yun Seob Lee <fhzotxldj@gmail.com>
2023-08-21 18:19:31 +0900

    update system info chart reusable

--- src/components/SystemInfoChart.tsx
+++ src/components/SystemInfoChart.tsx
@@ -2,26 +2,33 @@ import Gauge from "react-svg-gauge";
 import { PieChart } from "./PieChart";
 import { useAppSelector } from "../redux/hooks";
 import { selectSystemInfo } from "../redux/slice/systemInfoSlice";
+import React from "react";
+
+type SystemInfoChartProps = {
+  type: "CPU" | "GPU";
+  load: number
+  temp: number
+}
 
 function getHexColor(value: number) {
   let string = value.toString(16);
   return string.length === 1 ? "0" + string : string;
 }
 
-export const SystemInfoChart = () => {
+export const SystemInfoChart: React.FC<SystemInfoChartProps> = ({type, load, temp}) => {
   const { gpuLoad: gpu_load, gpuTemp: gpu_temp } =
     useAppSelector(selectSystemInfo);
-  let r = Math.floor(gpu_temp * 2.55);
-  let g = Math.floor(255 - gpu_temp * 2.55);
+  let r = Math.floor(temp * 2.55);
+  let g = Math.floor(255 - temp * 2.55);
   let b = 0;
   let colorHex = "#" + getHexColor(r) + getHexColor(g) + getHexColor(b);
 
   return (
     <div>
-      <h1>GPU</h1>
-      <PieChart data={gpu_load} />
+      <h1>{type}</h1>
+      <PieChart data={load} />
       <Gauge
-        value={gpu_temp}
+        value={temp}
         width={400}
         height={320}
         color={colorHex}
