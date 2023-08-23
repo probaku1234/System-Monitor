3614ee2bc939ffd8fc777b9f56f8b74d79ececde
Yun Seob Lee <fhzotxldj@gmail.com>
2023-08-23 15:59:10 +0900

    add test for systemInfoAPI

---
+++ src/redux/slice/systemInfoAPI.test.ts
@@ -0,0 +1,39 @@
+import { mockIPC } from "@tauri-apps/api/mocks";
+import { it, describe, beforeAll, expect, vi } from "vitest";
+import { fetchSystemInfo } from "./systemInfoAPI";
+
+beforeAll(() => {
+  mockIPC((cmd, _args) => {
+    // simulated rust command called "add" that just adds two numbers
+    if (cmd === "get_sys_info") {
+      return {
+        cpuName: "test cpu",
+        usedMemory: 1,
+        totalMemory: 5,
+        gpuName: "test gpu",
+        gpuTemp: 60,
+        gpuLoad: 5,
+        fetching: false,
+        error: "",
+      };
+    }
+  });
+});
+
+describe("systemInfoAPI", async () => {
+  it("should fetch system info", () => {
+    const spy = vi.spyOn(window, "__TAURI_IPC__");
+
+    expect(fetchSystemInfo()).resolves.toStrictEqual({
+      cpuName: "test cpu",
+      usedMemory: 1,
+      totalMemory: 5,
+      gpuName: "test gpu",
+      gpuTemp: 60,
+      gpuLoad: 5,
+      fetching: false,
+      error: "",
+    });
+    expect(spy).toHaveBeenCalled();
+  });
+});
