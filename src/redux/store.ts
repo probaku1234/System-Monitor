c08f0b29cb4b1de920c77083825995224c7e94c8
Yun Seob Lee <fhzotxldj@gmail.com>
2023-08-23 16:23:25 +0900

    refactor store for testing

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,9 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  ThunkAction,
+  Action,
+  PreloadedState,
+} from "@reduxjs/toolkit";
 import currentPageReducer from "./slice/currentPageSlice";
 import systemInfoReducer from "./slice/systemInfoSlice";
 
@@ -9,7 +14,17 @@ export const store = configureStore({
   },
 });
 
-export type AppStore = ReturnType<typeof configureStore>;
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: {
+      currentPage: currentPageReducer,
+      systemInfo: systemInfoReducer,
+    },
+    preloadedState,
+  });
+};
+
+export type AppStore = ReturnType<typeof setupStore>;
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
