17dd075b027b5e6805aee6dcd9e56c70da5c72cd
Yun Seob Lee <fhzotxldj@gmail.com>
2023-08-23 20:28:54 +0900

    add test for monitoring

---
+++ src/tests/pages/Monitoring.test.tsx
@@ -0,0 +1,45 @@
+import Monitoring from "../../pages/Monitoring";
+import { renderWithProviders } from "../../utils/test-utils";
+import { it, describe, beforeAll, expect, vi } from "vitest";
+import { screen, fireEvent, waitFor } from "@testing-library/react";
+import { setupStore } from "../../redux/store";
+import { setInfoAsync } from "../../redux/slice/systemInfoSlice";
+import { mockIPC } from "@tauri-apps/api/mocks";
+
+beforeAll(() => {
+  mockIPC((cmd, _args) => {
+    // simulated rust command called "add" that just adds two numbers
+    if (cmd === "get_sys_info") {
+      return {
+        cpuName: "test cpu",
+        usedMemory: 1,
+        totalMemory: 5,
+        gpuName: "test gpu",
+        gpuTemp: 60,
+        gpuLoad: 5,
+        fetching: false,
+        error: "",
+      };
+    }
+  });
+});
+
+describe("Monitoring", () => {
+  it("renders Monitoring", () => {
+    renderWithProviders(<Monitoring />);
+
+    expect(screen.getByText(/Monitoring/i)).toBeDefined();
+  });
+
+  it("renders system info", async () => {
+    const store = setupStore();
+    await waitFor(() => {
+      store.dispatch(setInfoAsync());
+    });
+
+    console.log(store.getState().systemInfo.cpuName);
+    renderWithProviders(<Monitoring />, { store });
+
+    expect(screen.getByText(/60/i)).toBeDefined();
+  });
+});
