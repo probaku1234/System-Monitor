aea2998583fcd8e19ef982d5153916d1397913cd
Yun Seob Lee <fhzotxldj@gmail.com>
2023-08-23 15:58:03 +0900

    set up test for redux

---
+++ src/utils/test-utils.tsx
@@ -0,0 +1,54 @@
+import React, { PropsWithChildren } from "react";
+import { render } from "@testing-library/react";
+import type { RenderOptions } from "@testing-library/react";
+import { configureStore } from "@reduxjs/toolkit";
+import type { PreloadedState } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+import type { AppStore, RootState } from "../redux/store";
+// As a basic setup, import your same slice reducers
+import currentPageReducer from "../redux/slice/currentPageSlice";
+import systemInfoReducer from "../redux/slice/systemInfoSlice";
+
+// This type interface extends the default options for render from RTL, as well
+// as allows the user to specify other things such as initialState, store.
+interface ExtendedRenderOptions extends Omit<RenderOptions, "queries"> {
+  preloadedState?: PreloadedState<RootState>;
+  store?: AppStore;
+}
+
+export function renderWithProviders(
+  ui: React.ReactElement,
+  {
+    preloadedState = {
+      currentPage: {
+        value: "home",
+      },
+      systemInfo: {
+        cpuName: "",
+        usedMemory: 0,
+        totalMemory: 0,
+        gpuLoad: 0,
+        gpuName: "",
+        gpuTemp: 0,
+        fetching: true,
+      },
+    },
+    // Automatically create a store instance if no store was passed in
+    store = configureStore({
+      reducer: {
+        currentPage: currentPageReducer,
+        systemInfo: systemInfoReducer,
+      },
+      preloadedState,
+    }),
+    ...renderOptions
+  }: ExtendedRenderOptions = {}
+) {
+  function Wrapper({ children }: PropsWithChildren<{}>): JSX.Element {
+    return <Provider store={store}>{children}</Provider>;
+  }
+
+  // Return an object with the store and all of RTL's query functions
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+}
