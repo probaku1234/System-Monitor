c9d47ef31d32a2df46e44a3265935bc426e661bf
Yun Seob Lee <fhzotxldj@gmail.com>
2023-08-25 12:15:12 +0900

    add show ram load on Monitoring page

--- src/pages/Monitoring.tsx
+++ src/pages/Monitoring.tsx
@@ -2,13 +2,20 @@ import { SystemInfoChart } from "../components/SystemInfoChart";
 import { useAppSelector } from "../redux/hooks";
 import { selectSystemInfo } from "../redux/slice/systemInfoSlice";
 import ClipLoader from "react-spinners/ClipLoader";
+import { GaugeChart } from "../components/GaugeChart";
 
 function Monitoring() {
   const {
     gpuLoad: gpu_load,
     gpuTemp: gpu_temp,
+    usedMemory,
+    totalMemory,
     fetching,
   } = useAppSelector(selectSystemInfo);
+
+  function getUsedMemoryPercentage() {
+    return Math.floor((usedMemory / totalMemory) * 100);
+  }
 
   return (
     <>
@@ -20,6 +27,8 @@ function Monitoring() {
         <>
           <SystemInfoChart type="GPU" load={gpu_load} temp={gpu_temp} />
           <SystemInfoChart type="CPU" load={0} temp={0} />
+          <h1>RAM</h1>
+          <GaugeChart temp={getUsedMemoryPercentage()} label="Load" unit="%" />
         </>
       )}
     </>
