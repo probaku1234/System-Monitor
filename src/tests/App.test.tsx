1dd79549459afb6e0383b45721910527253c00cd
Yun Seob Lee <fhzotxldj@gmail.com>
2023-08-26 14:10:27 +0900

    update app test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -3,6 +3,8 @@ import { mockIPC } from "@tauri-apps/api/mocks";
 import { renderWithProviders } from "../utils/test-utils";
 import { it, describe, beforeAll, expect, vi } from "vitest";
 import { screen, fireEvent, waitFor } from "@testing-library/react";
+import { setupStore } from "../redux/store";
+import { setInfoAsync } from "../redux/slice/systemInfoSlice";
 
 beforeAll(() => {
   mockIPC((cmd, _args) => {
@@ -31,17 +33,20 @@ describe("App", () => {
 
   it("should fetch system info", async () => {
     // const spy = vi.spyOn(window, "__TAURI_IPC__");
-
     // renderWithProviders(<App />);
-
     // await new Promise((r) => setTimeout(r, 2000));
     // await waitFor(() => {
     //   expect(spy).toHaveBeenCalled();
     // });
   });
 
-  it("should render pages by current page", () => {
-    renderWithProviders(<App />);
+  it("should render pages by current page", async () => {
+    const store = setupStore();
+    await waitFor(() => {
+      store.dispatch(setInfoAsync());
+    });
+
+    renderWithProviders(<App />, { store });
 
     fireEvent.click(screen.getByText(/System Spec/i));
     expect(screen.getByText(/CPU/i)).toBeDefined();
