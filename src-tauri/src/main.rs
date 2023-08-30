dd07e9da52a0c46d06a4acf326c85a1fd673ea8e
Yun Seob Lee <fhzotxldj@gmail.com>
2023-08-30 16:52:39 +0900

    add new command for sending hardware names

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,14 +2,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use crate::sys_info::SystemInfoFetcher;
-use log::info;
 use simple_logger::SimpleLogger;
 mod sys_info;
-// use lru::LruCache;
-// use std::num::NonZeroUsize;
-use tauri::{Manager, api::path::cache_dir, State};
-use std::{collections::HashMap, sync::Mutex};
-use systemstat::{saturating_sub_bytes, Platform, System};
+//use lru::LruCache;
+use std::num::NonZeroUsize;
+use tauri::State;
+use systemstat::{Platform, System};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -24,8 +22,15 @@ fn get_sys_info(sys: State<System>) -> sys_info::SystemInfo {
     system_info_fetcher.create_sys_info()
 }
 
+#[tauri::command]
+fn get_sys_spec_info(sys: State<System>) -> sys_info::SystemSpecInfo {
+    let system_info_fetcher = SystemInfoFetcher::new(sys.inner());
+
+    system_info_fetcher.create_sys_spec_info()
+}
+
 fn main() {
-    // let cache: LruCache<&str, &str> = LruCache::new(NonZeroUsize::new(10).unwrap());
+    //let cache: LruCache<&str, &str> = LruCache::new(NonZeroUsize::new(10).unwrap());
     SimpleLogger::new().init().unwrap();
     log::info!("tauri app started");
     tauri::Builder::default()
@@ -38,14 +43,14 @@ fn main() {
         //     Ok(())
         // })
         .on_window_event(|event| match event.event() {
-            tauri::WindowEvent::CloseRequested { api, .. } => {
+            tauri::WindowEvent::CloseRequested {  .. } => {
                 log::info!("tauri app ended");
-                
             }
             _ => {}
         })
         .manage(System::new())
-        .invoke_handler(tauri::generate_handler![greet, get_sys_info])
+        //.manage(cache)
+        .invoke_handler(tauri::generate_handler![greet, get_sys_info, get_sys_spec_info])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
