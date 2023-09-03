4034f7019993f15c4163709944b2ceb195d7b9a9
Yun Seob Lee <fhzotxldj@gmail.com>
2023-09-03 16:41:28 +0900

    implement system tray feature

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,8 +6,9 @@ use simple_logger::SimpleLogger;
 mod sys_info;
 //use lru::LruCache;
 use std::num::NonZeroUsize;
-use tauri::State;
 use systemstat::{Platform, System};
+use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu};
+use tauri::{Manager, State, SystemTrayEvent, SystemTrayMenuItem};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -30,10 +31,32 @@ fn get_sys_spec_info(sys: State<System>) -> sys_info::SystemSpecInfo {
 }
 
 fn main() {
-    //let cache: LruCache<&str, &str> = LruCache::new(NonZeroUsize::new(10).unwrap());
+    let show = CustomMenuItem::new("show".to_string(), "Show");
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(show)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(quit);
+    let system_tray = SystemTray::new().with_menu(tray_menu);
+
     SimpleLogger::new().init().unwrap();
     log::info!("tauri app started");
+
     tauri::Builder::default()
+        .system_tray(system_tray)
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "quit" => {
+                    std::process::exit(0);
+                }
+                "show" => {
+                    let window = app.get_window("main").unwrap();
+                    window.show().unwrap();
+                }
+                _ => {}
+            },
+            _ => {}
+        })
         .plugin(tauri_plugin_store::Builder::default().build())
         // .setup(|app| {
         //     let mut cache = LruCache::new(NonZeroUsize::new(2).unwrap());
@@ -43,14 +66,19 @@ fn main() {
         //     Ok(())
         // })
         .on_window_event(|event| match event.event() {
-            tauri::WindowEvent::CloseRequested {  .. } => {
-                log::info!("tauri app ended");
+            tauri::WindowEvent::CloseRequested { api, .. } => {
+                event.window().hide().unwrap();
+                api.prevent_close();
             }
             _ => {}
         })
         .manage(System::new())
         //.manage(cache)
-        .invoke_handler(tauri::generate_handler![greet, get_sys_info, get_sys_spec_info])
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            get_sys_info,
+            get_sys_spec_info
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
