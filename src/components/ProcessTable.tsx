a59fbb46754012bf949b06c49571208de8033973
Yun Seob Lee <fhzotxldj@gmail.com>
2023-09-08 20:50:39 +0900

    add pagination to process table

--- src/components/ProcessTable.tsx
+++ src/components/ProcessTable.tsx
@@ -1,7 +1,4 @@
-import {
-  Column,
-  CompactTable,
-} from "@table-library/react-table-library/compact";
+import { CompactTable } from "@table-library/react-table-library/compact";
 import { useTheme } from "@table-library/react-table-library/theme";
 import { getTheme } from "@table-library/react-table-library/baseline";
 import { useSort } from "@table-library/react-table-library/sort";
@@ -9,17 +6,22 @@ import { Action, State } from "@table-library/react-table-library/types/common";
 import { useAppSelector } from "../redux/hooks";
 import { selectSystemInfo } from "../redux/slice/systemInfoSlice";
 import { PorcessInfoState } from "../redux/slice/systemInfoSlice";
+import { usePagination } from "@table-library/react-table-library/pagination";
 import { byteToMByte } from "../utils/util-functions";
 import "./ProcessTable.css";
 
 export const ProcessTable = () => {
   const { processInfo } = useAppSelector(selectSystemInfo);
-  const nodes = processInfo.slice(0, 10);
+  const nodes = processInfo;
   const data = { nodes };
   const theme = useTheme(getTheme());
   const sort = useSort(
     data as any,
     {
+      state: {
+        sortKey: "CPU",
+        reverse: true,
+      },
       onChange: onSortChange,
     },
     {
@@ -30,8 +32,19 @@ export const ProcessTable = () => {
       },
     }
   );
+  const pagination = usePagination(data as any, {
+    state: {
+      page: 0,
+      size: 5,
+    },
+    onChange: onPaginationChange,
+  });
 
   function onSortChange(action: Action, state: State) {
+    console.log(action, state);
+  }
+
+  function onPaginationChange(action: Action, state: State) {
     console.log(action, state);
   }
 
@@ -43,18 +56,49 @@ export const ProcessTable = () => {
     },
     {
       label: "CPU",
-      renderCell: (item: PorcessInfoState) => `${item.cpuUsage} %`,
+      renderCell: (item: PorcessInfoState) => `${item.cpuUsage.toFixed(2)} %`,
       sort: { sortKey: "CPU" },
     },
     {
       label: "RAM",
-      renderCell: (item: PorcessInfoState) => `${byteToMByte(item.memoryUsage)} MB`,
+      renderCell: (item: PorcessInfoState) =>
+        `${byteToMByte(item.memoryUsage)} MB`,
       sort: { sortKey: "RAM" },
     },
   ];
   return (
     <>
-      <CompactTable columns={COLUMNS} data={data} theme={theme} sort={sort} />
+      <CompactTable
+        columns={COLUMNS}
+        data={data}
+        theme={theme}
+        sort={sort}
+        pagination={pagination}
+      />
+
+      <br />
+      <div className="pagination-wrapper">
+        <span>Total Pages: {pagination.state.getTotalPages(data.nodes)}</span>
+
+        <span>
+          Page:{" "}
+          {pagination.state
+            .getPages(data.nodes)
+            .map((_: PorcessInfoState, index: number) => (
+              <button
+                key={index}
+                type="button"
+                style={{
+                  fontWeight:
+                    pagination.state.page === index ? "bold" : "normal",
+                }}
+                onClick={() => pagination.fns.onSetPage(index)}
+              >
+                {index + 1}
+              </button>
+            ))}
+        </span>
+      </div>
     </>
   );
 };
